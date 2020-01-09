8529990b4ec9284e3a4d1736ff29ccd0844dac02
JaiLuthra1 <luthrajaiji@gmail.com>
2020-01-09 15:32:33 +0530

    New home

--- SIHweather/SIHweather/urls.py
+++ SIHweather/SIHweather/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from weather import views as w
 
 urlpatterns = [
+    path('',w.home),
     path('admin/', admin.site.urls),
 ]
