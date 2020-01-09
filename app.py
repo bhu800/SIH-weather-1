43613f03d88840a15a324e59589e4e840f3035af
JaiLuthra1 <luthrajaiji@gmail.com>
2020-01-09 14:20:32 +0530

    Use CITY_ID instead of city name

--- app.py
+++ app.py
@@ -1,25 +1,41 @@
 import requests
 
-dataget = requests.get('http://api.openweathermap.org/data/2.5/weather?appid=77763ccd48ec0f13fc6b857738eaa50c&q='+input())
-dataget=dataget.json()
+# dataget = requests.get('http://api.openweathermap.org/data/2.5/weather?appid=77763ccd48ec0f13fc6b857738eaa50c&q='+input())
+
+cityn = input()
+ci_id = -1000
+
+import json
+
+with open('city.list.json') as json_file:
+    data = json.load(json_file)
+    for dt in data:
+    	if(dt['name'].lower()==cityn.lower()):
+    		ci_id=dt['id']
+    		break
+    json_file.close()
 
 wea={}
-try:
-	w1=dataget['weather'][0]['main']
-	w2=dataget['weather'][0]['description']
-	w3=dataget['main']['temp']
-	w4=dataget['main']['pressure']
-	w5=dataget['main']['humidity']
-	w6=dataget['wind']['speed']
+if(ci_id != -1000):
+	try:
+		dataget = requests.get('http://api.openweathermap.org/data/2.5/weather?appid=77763ccd48ec0f13fc6b857738eaa50c&id='+str(ci_id))
+		dataget=dataget.json()
+		w1=dataget['weather'][0]['main']
+		w2=dataget['weather'][0]['description']
+		w3=dataget['main']['temp']
+		w4=dataget['main']['pressure']
+		w5=dataget['main']['humidity']
+		w6=dataget['wind']['speed']
 
-	wea["General"]=w1
-	wea["Description"]=w2
-	wea["Temperature"]=w3
-	wea["Pressure"]=w4
-	wea["Humidity"]=w5
-	wea["Speed"]=w6
+		wea["General"]=w1
+		wea["Description"]=w2
+		wea["Temperature"]=w3
+		wea["Pressure"]=w4
+		wea["Humidity"]=w5
+		wea["Speed"]=w6
 
-except:
-	pass
+	except:
+		pass
 
 print(wea)
+
